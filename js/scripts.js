a940d0712bb0bd228d6c630a63fc6a1cefa66659
Alex Fallenstedt and Schuyler Klaassen <schuyler.m.k@gmail.com>
2016-01-25 13:20:53 -0800

    links userinterface logic with business logic with function place

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,14 @@ var place = function(continent, landmark, notes) {
 
 
 $(document).ready(function() {
+  $("form#new-place").submit(function(event) {
+    event.preventDefault();
 
+    var inputtedContinent = $("input#new-continent").val();
+    var inputtedLandmark = $("input#new-landmark").val();
+    var inputtedNotes = $("input#new-notes").val();
+
+    var inputtedPlace = new place(inputtedContinent, inputtedLandmark, inputtedNotes);
+    
+  });
 });
